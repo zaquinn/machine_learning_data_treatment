d83c427ef086e5d1ad98729559ed8ad436d9aac9
isaac <ayzick120@gmail.com>
2023-03-19 20:34:11 -0300

    fix: removed venv folder, added gitignore file, started creating plots for BaseSensus class

---
+++ README.md
@@ -0,0 +1,3 @@
+## Projeto de Estudos de Machine Learning - Tratamento de Base de Dados
+
+Este é um projeto de estudos, que visa manipular as bases de dados que se encontram em formato .csv na raiz do projeto, visando a tratativa dos dados das mesmas.
--- base_census/base_census.py
+++ base_census/base_census.py
@@ -10,10 +10,18 @@ class BaseSensus:
     def execute():
         base_census = pd.read_csv("census.csv")
 
-        print(
-            base_census.describe()
-        )  # método util pra observar os dados e procurar valores inconsistentes, como valores negativos
+        # print(
+        #     base_census.describe()
+        # )  # método util pra observar os dados e procurar valores inconsistentes, como valores negativos
 
-        print(
-            base_census.isnull().sum()
-        )  # soma a quantidade de valores nulos/faltantes pra checar se a base de dados possui valores faltantes/nulos
+        # print(
+        #     base_census.isnull().sum()
+        # )  # soma a quantidade de valores nulos/faltantes pra checar se a base de dados possui valores faltantes/nulos
+
+        # print(np.unique(base_census["income"], return_counts=True))
+
+        # sns.countplot(x=base_census["income"]) # gera o plot especificado na variavel x
+
+        plt.hist(x=base_census["age"])  # gera o histograma especificado na variavel x
+
+        plt.show()  # exibe o plot do seaborn/pyplot etc
